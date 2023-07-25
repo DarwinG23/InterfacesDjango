9b7ccb5c9263d1bbe1b050f57c564ee6ba4eb851
DarwinG23 <131827617+DarwinG23@users.noreply.github.com>
2023-07-24 21:23:37 -0500

    Django

---
+++ myapp/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def inicio(request):
+    return render(request, 'inicio.html')
+
+def Principal(request):
+    return render(request, 'Principal.html')
